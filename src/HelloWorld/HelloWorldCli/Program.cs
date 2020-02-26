232dba890d06a61d2f36d4d3234864828e37da1d
Damon Gray <damongr@microsoft.com>
2020-02-26 12:11:34 -0800

    Initial version of our HelloWorld test app that has a cli, library and test project

---
+++ src/HelloWorld/HelloWorldCli/Program.cs
@@ -0,0 +1,15 @@
+﻿using HelloWorldLib;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorldCli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHelloWorldSpeaker speaker= new HelloWorldSpeaker(Console.Out);
+            await speaker.SayHelloAsync();
+        }
+    }
+}
---
+++ src/HelloWorld/HelloWorldLib/HelloWorldSpeaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HelloWorldLib
+{
+    public class HelloWorldSpeaker : IHelloWorldSpeaker
+    {
+        private TextWriter output;
+        public HelloWorldSpeaker(TextWriter output)
+        {
+            this.output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        public Task SayHelloAsync()
+        {
+            return output.WriteLineAsync("Hello, World!");
+        }
+    }
+}
